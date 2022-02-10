117abd6b9468dff92d196fa28d54264a0d2389d2
Lukasz <lukaszr500@gmail.com>
2022-02-10 16:54:56 +0100

    Github Action test

--- database-scripts/recreate.sh
+++ database-scripts/recreate.sh
@@ -3,7 +3,13 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 USER='dbuser'
 PASSWORD='dbpassword'
 DATABASE='test'
+if [ "$1" ]; then
+    HOST=$1
+else
+    HOST='localhost'
+fi
+
 
 cd $SCRIPT_DIR
-PGPASSWORD=$PASSWORD psql -h postgres -U $USER $DATABASE -tc "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\""
-PGPASSWORD=$PASSWORD psql -h postgres -U $USER $DATABASE -f delete.sql -f create.sql -f insert.sql
+PGPASSWORD=$PASSWORD psql -h $HOST -U $USER $DATABASE -tc "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\""
+PGPASSWORD=$PASSWORD psql -h $HOST -U $USER $DATABASE -f delete.sql -f create.sql -f insert.sql
